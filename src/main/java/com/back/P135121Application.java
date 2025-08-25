309d9ecd0dc84c6a2e713f7b9bce55158a84f14f
SWWWin <bgpd1258@naver.com>
2025-08-25 15:45:22 +0900

    feat: 개발 환경 셋팅

---
+++ src/main/java/com/back/P135121Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P135121Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P135121Application.class, args);
+    }
+
+}
